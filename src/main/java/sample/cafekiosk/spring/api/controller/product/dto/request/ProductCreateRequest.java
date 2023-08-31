a22349151dc74b0b7643fc0b227bd5de31574f3a
BonSik <rnqhstlr2297@gmail.com>
2023-08-31 18:12:53 +0900

    Presentation Layer test 주석 추가

--- src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -16,6 +16,12 @@ import javax.validation.constraints.Positive;
 @NoArgsConstructor
 public class ProductCreateRequest {
 
+    /**
+     * TODO: 읽기
+     * 기본 validation vs 도메인 validation 를 구분하자!!!!!!!!!!!
+     * 기본 validation (String 타입, 비었는지 등)은 컨트롤러에서 잡는게 맞다.(최소한의 검증!!)
+     * but) 도메인 validation(길이 등)은 "도메인(생성자 등)" or "서비스"에서 검증하는 걸로 분리해서 "책임"을 분리하는 것이 좋다.!!!
+     */
     @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
 
